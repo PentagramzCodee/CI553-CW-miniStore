0d99c393e3428ea13b6711ab59481e45428eb03b
PentagramzCodee <albertoeisa1997@outlook.com>
2025-01-10 14:46:08 +0000

    Update CashierController.java

--- clients/cashier/CashierController.java
+++ clients/cashier/CashierController.java
@@ -1,10 +1,9 @@
 package clients.cashier;
-
+import catalogue.Basket;
 
 /**
  * The Cashier Controller
  */
-
 public class CashierController
 {
   private CashierModel model = null;
@@ -12,7 +11,7 @@ public class CashierController
 
   /**
    * Constructor
-   * @param model The model 
+   * @param model The model
    * @param view  The view from which the interaction came
    */
   public CashierController( CashierModel model, CashierView view )
@@ -22,27 +21,55 @@ public class CashierController
   }
 
   /**
+   * Apply a 10% discount to the basket
+   */
+  public void doDiscount() {
+    Basket basket = model.getBasket();
+    if (basket != null && !basket.isEmpty()) {
+      // Calculate 10% discount and update the basket total
+      double discount = 0.10;
+      double originalTotal = basket.getTotalPrice();
+      double discountedTotal = originalTotal * (1 - discount);
+
+      basket.setTotalPrice(discountedTotal); // Update the total price in the basket
+      model.askForUpdate();  // Notify the model to update the view with the new total
+    }
+  }
+
+  /**
    * Check interaction from view
    * @param pn The product number to be checked
    */
-  public void doCheck( String pn )
+  public void doCheck(String pn)
   {
     model.doCheck(pn);
   }
 
-   /**
+  /**
    * Buy interaction from view
    */
   public void doBuy()
   {
     model.doBuy();
   }
-  
-   /**
+
+  /**
    * Bought interaction from view
    */
   public void doBought()
   {
     model.doBought();
+  }
+
+  /**
+   * Remove selected item from the basket.
+   * @param i The index of the item to be removed
+   */
+  public void doRemove(int i) {
+    Basket basket = model.getBasket();
+    if (basket != null && i >= 0 && i < basket.size()) {
+      basket.remove(i);  // Remove item at the specified index
+      model.askForUpdate();  // Notify the model to update the view
+    }
   }
 }
